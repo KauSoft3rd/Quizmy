ca0358bd9e60cbadbd351d033fdb1690cd50457e
Goosmos <bdt040321@gmail.com>
2024-03-08 13:26:05 +0900

    index 수정

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
