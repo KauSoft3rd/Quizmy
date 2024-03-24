a34d280c682e40930fb7920423798f4586300dad
Goosmos <bdt040321@gmail.com>
2024-03-24 20:02:19 +0900

    database 제거

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql';
-
-export const database = mysql.createConnection({
-    host: "quizmydb.cpiwaee4eftz.ap-northeast-2.rds.amazonaws.com",
-    user: "quizmy",
-    password: "eksrufthgkr2024!",
-    database: "QuizmyDB"
-})
-
