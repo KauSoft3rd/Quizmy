bb5553c22f727eff1cff02addfbf862b6f412568
Goosmos <bdt040321@gmail.com>
2024-05-06 15:51:42 +0900

    뉴스 스크랩 기능 수정

--- src/routes/news.router.js
+++ src/routes/news.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getNews, postBookmark, getMainNews, deleteBookmark, 
-    getBookmarkNews, getNaverNewsKeyword, getNewsKeyword, getMainNewsList } from '../controllers/news.controller';
+    getBookmarkNews, getNaverNewsKeyword, getNewsKeyword, getMainNewsList, getUserBookmark } from '../controllers/news.controller';
 import { deleteBookmarkMiddleware, postBookmarkMiddleware } from '../middleware/news.middleware';
 
 export const newsRouter = express.Router();
@@ -11,7 +11,7 @@ newsRouter.get('/', getNews); // clear
 newsRouter.get('/mainnews', getMainNews); // clear
 newsRouter.post('/bookmark', postBookmarkMiddleware, postBookmark); // clear
 newsRouter.delete('/bookmark', deleteBookmarkMiddleware, deleteBookmark); // clear
-newsRouter.get('/bookmark', getBookmarkNews); // clear
+newsRouter.get('/bookmark', getUserBookmark); // clear
 
 newsRouter.get('/keyword', getNewsKeyword); // 
 newsRouter.get('/keywordNews', getNaverNewsKeyword); //
