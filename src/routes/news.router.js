607a2284765239a429b71a4c8b4457f1dbc53c19
Goosmos <bdt040321@gmail.com>
2024-05-09 18:26:43 +0900

    add middle ware

--- src/routes/news.router.js
+++ src/routes/news.router.js
@@ -2,18 +2,19 @@ import express from 'express';
 import { getNews, postBookmark, getMainNews, deleteBookmark, 
     getBookmarkNews, getNaverNewsKeyword, getNewsKeyword, getMainNewsList, getUserBookmark } from '../controllers/news.controller';
 import { deleteBookmarkMiddleware, postBookmarkMiddleware } from '../middleware/news.middleware';
+import { kakaoIdToUserIdMiddleware } from "../middleware/user.middleware.js";
 
 export const newsRouter = express.Router();
 
-newsRouter.get('/', getNews); // clear
+newsRouter.get('/', kakaoIdToUserIdMiddleware, getNews); // clear
 
-// newsRouter.get('/', getMainNewsList); // clear
-newsRouter.get('/mainnews', getMainNews); // clear
-newsRouter.post('/bookmark', postBookmarkMiddleware, postBookmark); // clear
-newsRouter.delete('/bookmark', deleteBookmarkMiddleware, deleteBookmark); // clear
-newsRouter.get('/bookmark', getUserBookmark); // clear
+// newsRouter.get('/', kakaoIdToUserIdMiddleware, getMainNewsList); // clear
+newsRouter.get('/mainnews', kakaoIdToUserIdMiddleware, getMainNews); // clear
+newsRouter.post('/bookmark', kakaoIdToUserIdMiddleware, postBookmarkMiddleware, postBookmark); // clear
+newsRouter.delete('/bookmark', kakaoIdToUserIdMiddleware, deleteBookmarkMiddleware, deleteBookmark); // clear
+newsRouter.get('/bookmark', kakaoIdToUserIdMiddleware, getUserBookmark); // clear
 
-newsRouter.get('/keyword', getNewsKeyword); // 
-newsRouter.get('/keywordNews', getNaverNewsKeyword); //
+newsRouter.get('/keyword', kakaoIdToUserIdMiddleware, getNewsKeyword); // 
+newsRouter.get('/keywordNews', kakaoIdToUserIdMiddleware, getNaverNewsKeyword); //
 
-newsRouter.get('/test', getMainNewsList);
+newsRouter.get('/test', kakaoIdToUserIdMiddleware, getMainNewsList);
