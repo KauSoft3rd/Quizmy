619f544e8ebaead9ffe3282807c1a119b273893d
Goosmos <bdt040321@gmail.com>
2024-05-22 00:59:18 +0900

    단어 되돌아보기 기능 수정

--- src/models/remind.dao.js
+++ src/models/remind.dao.js
@@ -49,35 +49,175 @@ try {
 
 // 사용자가 시도한 단어를 최신순 조회
 export const getNewestRemindDao = async (user_id) => {
-try {
-    const db = await pool.getConnection();
-    const [userRemindList] = await db.query(getNewestRemindListSql, user_id);
-    console.log(userRemindList);
-    const remindList = await Promise.all(userRemindList.map(async (item) => {
-        const [info] = await db.query(getWordInfoSql, [item.words_id]);
-        return {item, info};
-    }))
-    db.release();
-    return remindList;
-} catch ( error ) {
-    return error;
-}
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(getNewestRemindListSql, user_id);
+        console.log(userRemindList);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return {item, info};
+        }))
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
 }
 
 // 사용자가 시도한 단어를 가나다순 조회
 export const getAlphaRemindDao = async (user_id) => {
-try {
-    const db = await pool.getConnection();
-    const [userRemindList] = await db.query(getUserRemindListSql, [user_id]);
-    const remindList = await Promise.all(userRemindList.map(async (item) => {
-        const [info] = await db.query(getWordInfoSql, [item.words_id]);
-        return { item, info };
-    }))
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(getUserRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }))
 
-    remindList.sort((a, b) => a.info.word.localCompare(b.info.word));
-    db.release();
-    return remindList;
-} catch ( error ) {
-    return error;
+        remindList.sort((a, b) => a.info.word.localCompare(b.info.word));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
 }
+
+
+// 오늘 시도한 단어를 최신순으로 조회
+import { todayNewestRemindListSql } from '../models/remind.sql';
+export const getTodayNewestRemindDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(todayNewestRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }))
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+
+// 누적 시도한 단어를 최신순으로 조회
+import { accNewestRemindListSql } from "./remind.sql";
+export const getAccNewestRemindDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(accNewestRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+
+// 오늘 시도한 단어중 정답을 조회
+import { todayCorrectRemindListSql } from './remind.sql';
+export const getTodayCorrectRemindListDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(todayCorrectRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+
+// 오늘 시도한 단어중 오답을 조회
+import { todayIncorrectRemindListSql } from './remind.sql';
+export const getTodayIncorrectRemindListDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(todayIncorrectRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+
+// 누적 시도한 단어중 정답을 조회
+import { accCorrectRemindListSql } from "./remind.sql";
+export const getAccCorrectRemindListDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(accCorrectRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+// 누적 시도한 단어중 오답을 조회
+import { accIncorrectRemindListSql } from "./remind.sql";
+export const getAccIncorrectRemindListDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(accIncorrectRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+
+export const getTodayAlphaRemindListDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(todayRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        remindList.sort((a, b) => a.info.word.localCompare(b.info.word));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
+}
+
+export const getAccAlphaRemindListDao = async (user_id) => {
+    try {
+        const db = await pool.getConnection();
+        const [userRemindList] = await db.query(getUserRemindListSql, [user_id]);
+        const remindList = await Promise.all(userRemindList.map(async (item) => {
+            const [info] = await db.query(getWordInfoSql, [item.words_id]);
+            return { item, info };
+        }));
+        remindList.sort((a, b) => a.info.word.localCompare(b.info.word));
+        db.release();
+        return remindList;
+    } catch ( error ) {
+        return error;
+    }
 }
