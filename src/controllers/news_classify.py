68c9c30928acf173e091057c67f7bb1771cb9ed2
Goosmos <bdt040321@gmail.com>
2024-05-30 21:43:10 +0900

    .py파일 수정

--- src/controllers/news_classify.py
+++ src/controllers/news_classify.py
@@ -9,26 +9,40 @@ from tensorflow.keras.preprocessing.text import tokenizer_from_json
 import json
 
 
-currentDir = os.path.dirname(os.path.abspath(__file__))
-modelPath = os.path.join(currentDir, 'news_classify.h5')
-tokenPath = os.path.join(currentDir, 'tokenizer.json')
+# currentDir = os.path.dirname(os.path.abspath(__file__))
+# modelPath = os.path.join(currentDir, 'news_classify.h5')
+# tokenPath = os.path.join(currentDir, 'tokenizer.json')
 
-# 모델과 토크나이저 불러오기
-model = load_model(modelPath)
+# # 모델과 토크나이저 불러오기
+# model = load_model(modelPath)
 
-# 토크나이저 불러오기
-with open(tokenPath) as f:
-    data = json.load(f)
-    tokenizer = tokenizer_from_json(data)
+# # 토크나이저 불러오기
+# with open(tokenPath) as f:
+#     data = json.load(f)
+#     tokenizer = tokenizer_from_json(data)
 
 def predict_news_category(news_title):
+
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    modelPath = os.path.join(currentDir, 'news_classify.h5')
+    tokenPath = os.path.join(currentDir, 'tokenizer.json')
+
+    # 모델과 토크나이저 불러오기
+    model = load_model(modelPath)
+
+    # 토크나이저 불러오기
+    with open(tokenPath) as f:
+        data = json.load(f)
+        tokenizer = tokenizer_from_json(data)
+
     news_sequence = tokenizer.texts_to_sequences([news_title])
     news_sequence = pad_sequences(news_sequence, maxlen=10, padding='post')
+    
     prediction = model.predict(news_sequence, verbose = 0)[0][0]
     return prediction >= 0.6
 
 if __name__ == '__main__':
-    news_title = sys.argv[1]
+    news_title = ' '.join(sys.argv[1:]) # 2140 수정
     prediction = predict_news_category(news_title)
     if prediction == True:
         sys.exit(1)
